ece047b439a34411a1b08318316523a89958f6b8
suxdygd <159431004+suxdygd@users.noreply.github.com>
2024-05-30 11:01:33 +0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,3 +2,6 @@ function sup() {
   window.location.href = 'https://patreon.com/SuxWebPatSupport';
 
 }
+function ytlink() {
+window.location.hred = 'https://www.youtube.com/channel/UC567lBtzL7NNohOev-2gAnw';
+}
